6905dd05f4552aa6941efd2797f31e87d7611ef0
Pasindu Madusanka <pasindusenerath@gmail.com>
2019-01-23 23:34:59 +0530

    add first component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,13 @@
-console.log("It works!");
+import React from "react";
+import { render } from "react-dom";
+
+class App extends React.Component {
+    render() {
+        return (
+        <div>
+            <h1>hello world </h1>
+        </div>
+        );
+    }
+}
+render(<App/>,window.document.getElementById("app"));
